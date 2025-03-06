2f5ba0dd02edbc8d7c2802ea2aef2e5cd9267771
sachinjaiswalnitr <jaiswalsachings@gmail.com>
2025-03-06 19:35:09 +0530

    skj

---
+++ api/app.js
@@ -0,0 +1,8 @@
+import express from "express";
+const app=express();
+app.use("/api/test",(req,res)=>{
+    res.send("it works")
+})
+app.listen(8800,()=>{
+    console.log("server is running");
+});
